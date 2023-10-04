cc7a2f0fa094fa4aa712bcf1d58dc32b04f890db
Amina Seye <aminaseye2@icloud.com>
2023-10-04 14:49:54 +0200

    (Create) Login and register

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,8 +1,72 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+import Elipse from "../../assets/Ellipse6.png";
+import { useForm } from "react-hook-form";
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const { handleSubmit, register } = useForm();
 
-export default Login
+  const onSubmit = handleSubmit((data) => console.log(data));
+
+  return (
+    <Box
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      sx={{
+        backgroundImage: `url(${Elipse})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        height="50%"
+        width="100%"
+        gap="15px"
+      >
+        <Typography variant="h4" textAlign="start" width="80%">
+          Login
+        </Typography>
+        <Box
+          component="form"
+          display="flex"
+          flexDirection="column"
+          width="80%"
+          gap="30px"
+          onSubmit={onSubmit}
+        >
+          <TextField
+            id="standard-basic"
+            label="EMAIL"
+            variant="standard"
+            {...register("email")}
+          />
+          <TextField
+            id="standard-basic"
+            type="password"
+            label="PASSWORD"
+            {...register("password", {
+              required: true,
+              minLength: 6,
+            })}
+            variant="standard"
+          />
+          <Typography textAlign="end">Forget password?</Typography>
+          <Typography textAlign="center">
+            You do not have an account <NavLink to="/register">Signup</NavLink>
+          </Typography>
+          <Button variant="contained" type="submit">
+            LOGIN
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Login;
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,9 +1,74 @@
-
+import { Box, Button, TextField, Typography } from "@mui/material";
+import Elipse from "../../assets/Ellipse6.png";
+import { useForm } from "react-hook-form";
+import { NavLink } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const { handleSubmit, register } = useForm();
+  const onSubmit = handleSubmit((data) => console.log(data));
 
-export default Register
+  const inputs = [
+    { label: "First Name", name: "firstName", type: "text" },
+    { label: "Last Name", name: "lastName", type: "text" },
+    { label: "Email", name: "email", type: "text" },
+    { label: "Password", name: "password", type: "password" },
+  ];
+
+  return (
+    <Box
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      sx={{
+        backgroundImage: `url(${Elipse})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        height="50%"
+        width="100%"
+        gap="15px"
+      >
+        <Typography variant="h4" textAlign="start" width="80%">
+          Register
+        </Typography>
+        <Box
+          component="form"
+          display="flex"
+          flexDirection="column"
+          width="80%"
+          gap="30px"
+          onSubmit={onSubmit}
+        >
+          {inputs.map((element) => {
+            console.log(element);
+            return (
+              <TextField
+                id="standard-basic"
+                label={element.label}
+                variant="standard"
+                {...register(element.name)}
+              />
+            );
+          })}
+
+          <Typography textAlign="center">
+            Do you already have an account?{" "}
+            <NavLink to="/login">Log in</NavLink>
+          </Typography>
+          <Button variant="contained" type="submit">
+            Create Account
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Register;
